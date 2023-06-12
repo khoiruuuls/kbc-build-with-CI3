a77ef83344d2fa161595d68bc48c854378120ad4
FadliArifRamadan15 <fadli.ramadhan.ti.20@cic.ac.id>
2023-06-12 11:00:21 +0700

    tambah navbar dan sidebar dipage dashboard consult

--- application/views/main/dashboard/index.php
+++ application/views/main/dashboard/index.php
@@ -1,24 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php $this->load->view("_partials/head.php") ?>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php echo ('assets/css/_partials/consultant.css'); ?>">
 </head>
+
 <body>
-    <h1>Selamat datang <?= $this->session->userdata('name') ?></h1>
-    <p>dashboard consultant</p>
-    <br>
-    <h4>Status profile <?= $profile * 10 ?></h4>
+    <!-- Perubahan Fadli -->
+    <!-- Tambah tampilan navbar dan sidebar kedalam dashboard konsultan -->
+    <section id="consultant-dashboard">
+        <div class="container-fluid">
+            <div class="row consultan-container">
+                <?php $this->load->view("_partials/navbar_consultant.php") ?>
+                <div class="col-3">
+                    <?php $this->load->view("_partials/sidebar_consultant.php") ?>
+                </div>
+                <div class="col">
+                    <div class="container-content">
+                        <div class="consultant-header">
+                            <h1>Selamat datang <?= $this->session->userdata('name') ?></h1>
+                            <p>dashboard consultant</p>
+                        </div>
+                        <div class="status-content">
+                            <h4>Status profile <?= $profile * 10 ?></h4>
+                        </div>
+                        <div class="program-content">
+                            <h1>Program aktif</h1>
+                            <h1>0</h1>
+
+                            <h1>Program waiting</h1>
+                            <h1>0</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 
-    <h1>Program aktif</h1>
-    <h1>0</h1>
-
-    <br>
-    <h1>Program waiting</h1>
-    <h1>0</h1>
 </body>
+
 </html>
